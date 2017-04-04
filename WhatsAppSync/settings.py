5d46ff23c3f261d67a0b099092c89a751d558428
Kenneth <matovken@gmial.com>
2017-04-04 11:52:05 +0300

    adding django-nose

--- WhatsAppSync/settings.py
+++ WhatsAppSync/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_nose',
+    'sync'
 ]
 
 MIDDLEWARE = [
@@ -118,3 +120,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+
+TEST_RUNNER = 'django_nose.NoseTestSuiteRunner'
--- sync/tests.py
+++ sync/tests.py
@@ -1,1 +1,8 @@
+from django.test import TestCase
+
 __author__ = 'kenneth'
+
+
+class DumbTest(TestCase):
+    def test_one_plus_one(self):
+        self.assertEquals(1+1, 2)
