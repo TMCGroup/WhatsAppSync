2454ca323dd6d734d268d5ec52f724b467a538b7
Nicholas Turner <mugtrade@gmail.com>
2019-01-14 17:25:14 +0300

    Select folder to get downloads from

--- WhatsAppSync/settings.py
+++ WhatsAppSync/settings.py
@@ -10,7 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/1.10/ref/settings/
 """
 import os
-
+from .envvars import *
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -77,7 +77,6 @@ WSGI_APPLICATION = 'WhatsAppSync.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
-
 
 DATABASES = {
     'default': {
